2856707614461522594dcff94699985dd466c5fc
Nikita Demidovich <nikita.demidovich@ccsteam.ru>
2022-07-05 14:55:58 +0300

    fix: imports in __init__.py

--- pybotx_smart_logger/__init__.py
+++ pybotx_smart_logger/__init__.py
@@ -1,8 +1,21 @@
-from .contextmanager import wrap_system_event
-from .exception_handler import (
+from pybotx_smart_logger.contextmanager import wrap_system_event
+from pybotx_smart_logger.exception_handler import (
     fastapi_exception_handler,
     make_smart_logger_exception_handler,
 )
-from .logger import smart_log
-from .middlewares import BotXSmartLoggerMiddleware, FastApiSmartLoggerMiddleware
-from .task_decorator import make_smart_logger_decorator
+from pybotx_smart_logger.logger import smart_log
+from pybotx_smart_logger.middlewares import (
+    BotXSmartLoggerMiddleware,
+    FastApiSmartLoggerMiddleware,
+)
+from pybotx_smart_logger.task_decorator import make_smart_logger_decorator
+
+__all__ = (
+    "BotXSmartLoggerMiddleware",
+    "fastapi_exception_handler",
+    "FastApiSmartLoggerMiddleware",
+    "make_smart_logger_decorator",
+    "make_smart_logger_exception_handler",
+    "smart_log",
+    "wrap_system_event",
+)
